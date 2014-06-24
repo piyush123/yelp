b9a4fce1dd3fc49b3620cc2a76b9f295de3e3342
piyush123 <piyush@onor.net>
2014-06-24 06:11:38 -0700

    ew

---
+++ Yelp/NSDictionary+setDictionary.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+setDictionary.h
+//  Yelp
+//
+//  Created by piyush shah on 6/22/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (setDictionary)
+
+@end
---
+++ Yelp/NSDictionary+setDictionary.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+setDictionary.m
+//  Yelp
+//
+//  Created by piyush shah on 6/22/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import "NSDictionary+setDictionary.h"
+
+@implementation NSDictionary (setDictionary)
+
+@end
